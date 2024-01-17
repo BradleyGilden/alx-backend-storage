9c6cfa3209f51fce14c16d26208e7e499a9a3282
BradleyGilden <bradleygilden@gmail.com>
2024-01-17 12:16:15 +0200

    added database

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,9 @@
 -- Make an attribute unique directly in the table schema
 
+-- Ensure database exists
+CREATE DATABASE IF NOT EXISTS holberton;
 -- Creating Table
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS holberton.users (
     id AUTO_INCREMENT NOT NULL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
