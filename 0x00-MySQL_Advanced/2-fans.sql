74483e7844965934d542d00af90d23057830c89c
BradleyGilden <bradleygilden@gmail.com>
2024-01-17 16:30:49 +0200

    filtering methods

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,5 @@
+-- ranks country origins of bands, ordered by the number of (non-unique) fans
+-- basic select statement to get specific columns
+SELECT origin, SUM(fans) AS nb_fans FROM holberton.metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
