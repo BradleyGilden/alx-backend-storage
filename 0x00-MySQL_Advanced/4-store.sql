99f27a049a3d9d0d3b02a036da2ce1e16176e298
BradleyGilden <bradleygilden@gmail.com>
2024-01-17 19:00:52 +0200

    remove specificity

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,10 @@
 -- a trigger that decreases the quantity of an item after adding a new order
 -- creating the trigger
 CREATE TRIGGER decrease
-AFTER INSERT ON holberton.orders
+AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    UPDATE holberton.items
+    UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
 END;
