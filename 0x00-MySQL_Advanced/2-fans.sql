cd3d9e734f437f620dcae136e0045eb848aebc31
BradleyGilden <bradleygilden@gmail.com>
2024-01-17 18:58:56 +0200

    remove specificity

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
 -- basic select statement to get specific columns
-SELECT origin, SUM(fans) AS nb_fans FROM holberton.metal_bands
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
