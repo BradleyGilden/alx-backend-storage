801b3d9af76d1d4ef0c79bcf807e70abe991939e
BradleyGilden <bradleygilden@gmail.com>
2024-01-17 18:59:50 +0200

    remove specificity

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
 -- select statement to view longevity data
 SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan
-FROM holberton.metal_bands
+FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
