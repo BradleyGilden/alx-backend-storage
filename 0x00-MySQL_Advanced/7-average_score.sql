f2fd727dd2a3d934633c79cd269d2e9c5562ae95
BradleyGilden <bradleygilden@gmail.com>
2024-01-18 11:27:48 +0200

    remove int declaration

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,7 +3,7 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score INT;
+    DECLARE avg_score;
     -- calculate project mark average of given user_id
     SELECT AVG(score) INTO avg_score FROM corrections
     WHERE corrections.user_id = user_id;
