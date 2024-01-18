24c2062fa28d4499f6a9530e275b32ab78d85c4d
BradleyGilden <bradleygilden@gmail.com>
2024-01-18 15:50:50 +0200

    formatting

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,8 +6,8 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser (
 )
 BEGIN
     UPDATE users
-   	SET average_score=(SELECT AVG(score) FROM corrections
-			     WHERE corrections.user_id=user_id)
+    SET average_score=(SELECT AVG(score) FROM corrections
+	                    WHERE corrections.user_id=user_id)
 	WHERE id=user_id;
 END $$
 
