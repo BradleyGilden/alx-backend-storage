ca483ffbf89eba08133e05f7f849a730cd9a861a
BradleyGilden <bradleygilden@gmail.com>
2024-01-18 11:29:43 +0200

    removed need for variables

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,12 +3,9 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score;
-    -- calculate project mark average of given user_id
-    SELECT AVG(score) INTO avg_score FROM corrections
-    WHERE corrections.user_id = user_id;
-    -- update user table with average score
-    UPDATE users SET average_score = avg_score
-    WHERE id = user_id;
+    UPDATE users
+    SET average_score=(SELECT AVG(score) FROM corrections
+    WHERE corrections.user_id=user_id)
+    WHERE id=user_id;
 END $$
 DELIMITER ;
