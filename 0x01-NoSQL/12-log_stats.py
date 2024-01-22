7cba84deb712ed235af49d974f0ec10e69fb761d
BradleyGilden <bradleygilden@gmail.com>
2024-01-22 14:54:35 +0200

    reformat

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,17 +7,24 @@ Date: 22-01-2024
 from pymongo import MongoClient
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient("mongodb://127.0.0.1:27017")
     # get nginx collection
     nginx = client.logs.nginx
-    logs = list(nginx.aggregate([
-        {"$group": {"_id": "$method", "count": {"$sum": 1}}},
-        {"$match": {"_id": {"$in": methods}}}
-    ]))
+    logs = list(
+        nginx.aggregate(
+            [
+                {"$group": {"_id": "$method", "count": {"$sum": 1}}},
+                {"$match": {"_id": {"$in": methods}}},
+            ]
+        )
+    )
     print("Methods:")
     for method in methods:
         log = next(filter(lambda x: x.get("_id") == method, logs), None)
-        (print(f"\tmethod {method}: {log['count']}")
-         if log else print(f"\tmethod {method}: 0"))
+        (
+            print(f"\tmethod {method}: {log['count']}")
+            if log
+            else print(f"\tmethod {method}: 0")
+        )
