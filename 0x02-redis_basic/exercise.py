85155ece7d58f30704bc47baaf35f0a6b5a4ef26
BradleyGilden <bradleygilden@gmail.com>
2024-01-24 12:21:48 +0200

    simple exercise

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+Author: Bradley Dillion Gilden
+Date: 24-01-2024
+"""
+from redis import Redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache:
+    """controls cache of an application"""
+
+    def __init__(self) -> None:
+        """constructor function"""
+        self._redis = Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """stores a value assigned to a random uuid generated key"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
+
+
+if __name__ == '__main__':
+    cache = Cache()
+
+    data = b"hello"
+    key = cache.store(data)
+    print(key)
+
+    local_redis = Redis()
+    print(local_redis.get(key))
